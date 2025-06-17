cc00c3f5c4dceec1185dcfb5bf70bced5cfadbf0
Vjosa <vjosa.fusha@gmail.com>
2025-06-17 15:17:23 +0200

    add auth backend and backend role for github

---
+++ vault/github-oidc.tf
@@ -0,0 +1,25 @@
+locals {
+  name = "oidc-demo"
+  company = "mycompany"
+}
+
+resource "vault_jwt_auth_backend" "oidc_demo" {
+  description        = "Accept OIDC authentication from GitHub Action workflows"
+  path               = local.name
+  oidc_discovery_url = "https://token.actions.githubusercontent.com"
+  bound_issuer       = "https://token.actions.githubusercontent.com"
+}
+
+resource "vault_jwt_auth_backend_role" "oidc_demo" {
+  role_name         = local.name
+  bound_claims_type = "string"
+  bound_subject     = "repo:${local.company}/${local.name}:ref:refs/heads/main"
+
+  # Required configuration attributes
+  token_policies  = [vault_policy.oidc_demo.name]
+  bound_audiences = ["https://github.com/${local.company}"]
+  role_type       = "jwt"
+  backend         = vault_jwt_auth_backend.oidc_demo.path
+  user_claim      = "actor"
+  token_ttl       = 300 # 5 minutes
+}
